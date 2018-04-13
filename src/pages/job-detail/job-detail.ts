618cda28b6d3801558737e2145f2246db8ece51c
rajashekar <raj003@stepfourward.in>
2018-04-13 22:26:06 +0530

    13 apr final commit

--- src/pages/job-detail/job-detail.ts
+++ src/pages/job-detail/job-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 
 /**
  * Generated class for the JobDetailPage page.
@@ -15,11 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class JobDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public actionSheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JobDetailPage');
+  }
+  moreOptions() {
+
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Additional Options',
+      
+      buttons: [
+        {
+          text: 'Save Job',
+          
+        },
+        {
+          text: 'Share'
+        },
+        {
+          text: 'Flag as Spam',
+          role: 'destructive'
+        },
+        {
+          text: 'cancel',
+          role: 'cancel'
+        }
+
+      ]
+    });
+    actionSheet.present();
+
   }
 
 }
