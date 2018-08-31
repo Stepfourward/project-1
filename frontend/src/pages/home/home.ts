51fea2da33f24ddf44f989c11429055b61b67fcf
rajashekar <raj003@stepfourward.in>
2018-08-31 16:57:29 +0530

    linkedin functionality - 1 added

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { NavController,Platform, IonicPage  } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { LoginPage } from '../login/login';
 import { LocationPage} from '../location/location';
@@ -8,53 +8,55 @@ import { TermsofusagePage } from '../termsofusage/termsofusage';
 import { LinkedIn, LinkedInLoginScopes } from '@ionic-native/linkedin';
 import { AuthService } from '../../services/auth.service';
 import { AlertController } from 'ionic-angular';
+import {Http, Headers} from '@angular/http';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
-import { Http,Headers } from '@angular/http';
+import { IonicPageModule  } from 'ionic-angular';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 declare var window: any;
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
   scopes: LinkedInLoginScopes[] = ['r_basicprofile', 'r_emailaddress', 'rw_company_admin', 'w_share'];
   isLoggedIn: boolean = false;
   selfData = { id:"", firstName:"", lastName:"", emailAddress:"" };
   clientId = '81owhlvqu6ukaj';
   clientSecret = '84AtzJD1H7YKiVIl';
-  redirect_uri = "sf://sfapp.com/callback";
+  redirect_uri = "http://localhost";
   state = 'hjfdhjGj12j';
   appScope = 'r_basicprofile';
+  userData: any;
+  linkedinURL: any;
+  accessToken: any;
+  result: any;
 
   constructor(public navCtrl: NavController, private  linkedin: LinkedIn, private authservices: AuthService,
-    private alertCtrl: AlertController, private http: Http, private platform: Platform,
-    private iab: InAppBrowser) {
+  private alertCtrl: AlertController, private http: HttpClient, private platform: Platform,
+  private iab: InAppBrowser) {
 
     this.platform = platform;
     this.http = http;
+    this.linkedinURL = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id='+this.clientId+'&redirect_uri='+this.redirect_uri+'&state='+this.state+'&scope='+this.appScope;
 
   }
 
   ionViewDidLoad() {
-    // this.linkedin.hasActiveSession().then((active) => {
-    //   this.isLoggedIn = active;
-    //   if(this.isLoggedIn === true) {
-    //     this.getSelfData();
-    //   }
-    // });
-    window.open('sf://sf-demoapp.com');
+    
   }
   
   gotoSignpage() {
 
-  	this.navCtrl.push(LoginPage);
+    this.navCtrl.push(LoginPage);
 
   }
 
   openRegisterPage() {
-  	this.navCtrl.push(RegisterPage);
+    this.navCtrl.push(RegisterPage);
   }
 // redirect to policy page
   toPolicypage() {
@@ -63,49 +65,46 @@ export class HomePage {
   totermPage() {
     this.navCtrl.push(TermsofusagePage);
   }
-  // linkedin login
+  
+  
   linkedinLogin() {
-    this.platform.ready().then(() => {
-      this.linkedinPage().then(success => {
-        alert(success.access_token);
-      },(error) => {
-        alert(error);
-      });
-    });
-  }
-
-  linkedinPage(): Promise<any> {
-    var self = this;
-    return new Promise((resolve, reject) => {
-      var browserRef = window.cordova.iab.open('https://www.linkedin.com/uas/oauth2/authorization?client_id=' + this.clientId + '&redirect_uri=' + this.redirect_uri + '&scope=' + this.appScope + '&response_type=code&state=' + this.state, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-      browserRef.addEventListener("loadstart", (event) => {
-        if((event.url).indexOf(this.redirect_uri) === 0) {
-          try {
-            var requestToken = (event.url).split("code=")[1].split("&")[0];
-            let headers = new Headers();
-            headers.append('Content-Type', 'application/x-www-form-urlencoded');
-            this.http.post("https://www.linkedin.com/oauth/v2/accessToken?client_id='81owhlvqu6ukaj'&client_secret='84AtzJD1H7YKiVIl'&redirect_uri=" + this.redirect_uri + "&grant_type=authorization_code&code=" + requestToken,
-            {headers: headers})
-            .subscribe(function(data){
-              resolve(data);
-              alert(data);
-            })
-          } catch(e) {
-              setTimeout(function() {
-                browserRef.close();
-              }, 10);
-            }
+    return new Promise((resolve,reject) => {
+      let browser = this.iab.create(this.linkedinURL,'_blank');
+      let listner = browser.on('loadstart').subscribe((event: any) => {
+        if(event.url.indexOf('oauth/v2/authorization') > -1){
+          return;
         }
-        else {
-          browserRef.addEventListener("exit", function(event) {
-            reject("The linkedin sign in flow was canceled");
-        });
-        } 
-
-      });
-      
-
-  });
+        if(event.url.indexOf(this.redirect_uri) > -1 ){
+          listner.unsubscribe();
+          browser.close();
+          let token = event.url.split('=')[1].split('&')[0];
+          this.accessToken = token;
+          resolve(event.url);
+          let headers = new Headers();
+          headers.append('Content-Type', 'application/x-www-form-urlencoded');
+          this.http.post<any>('https://www.linkedin.com/oauth/v2/accessToken?client_id='+this.clientId+'&client_secret='+this.clientSecret+'&grant_type=authorization_code&code='+this.accessToken+'&redirect_uri='+this.redirect_uri,{headers: headers})
+          .subscribe(res => {
+            this.result = res.json();
+          } 
+          );
+          this.gettheLinkedinUserDetails(this.result["access_token"])
+        } else {
+          reject("Could not authenticate");
+        }
+      })
+    })
   }
 
+  gettheLinkedinUserDetails(token: string) {
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('Authorization','Bearer '+token);
+    this.http.get('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url)?format=json',{headers: headers})
+    .subscribe(profileData => {
+      alert(profileData + 'hii');
+    })
+  }
+
+
+  
 }
